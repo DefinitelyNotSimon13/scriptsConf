cf7f7bcb131c2daede5d567a776a49b932ad3d55
DefinitelyNotSimon13 <simon21.blum@gmail.com>
2024-02-01 10:07:29 +0100

    Add runWithDots.sh script

---
+++ utils/runWithDots.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# source: https://stackoverflow.com/questions/25903943/looping-dots-until-a-process-is-complete-bash
+
+"$@" &
+runningPrinted=false
+while kill -0 $!; do
+  if [[ "$runningPrinted" == "false" ]]; then
+    printf "Running" > /dev/tty
+    runningPrinted=true
+  fi
+  printf '.' > /dev/tty
+  printf "$counter" > /dev/tty
+  sleep 1
+done
+
+printf '\n' > /dev/tty
+
